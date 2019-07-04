bb21386b49c259f253382b5b3a74b238049ec61f
Nitu Stelian <stelican@gmail.com>
2019-07-04 17:54:04 +0300

    sufix != prefix

--- src/main/java/ro/itschool/mvnbase/tema9/UpperCaseSuffixFormatter.java
+++ src/main/java/ro/itschool/mvnbase/tema9/UpperCaseSuffixFormatter.java
@@ -2,6 +2,6 @@ package ro.itschool.mvnbase.tema9;
 
 public class UpperCaseSuffixFormatter extends UpperCaseFormatter {
     public String format(String text, String customSuffix) {
-        return customSuffix + text.toUpperCase();
+        return text.toUpperCase()+ customSuffix;
     }
 }
