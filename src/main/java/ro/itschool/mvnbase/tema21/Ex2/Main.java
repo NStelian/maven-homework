0cb1c1353491f9681f7bb6d36cab7af5b3302639
Nitu Stelian <stelican@gmail.com>
2019-08-20 14:24:59 +0300

    extra

--- src/main/java/ro/itschool/mvnbase/tema21/Ex2/Main.java
+++ src/main/java/ro/itschool/mvnbase/tema21/Ex2/Main.java
@@ -1,6 +1,7 @@
 package ro.itschool.mvnbase.tema21.Ex2;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -58,5 +59,17 @@ public class Main {
                 .map(job -> job.getDeadLine())
                 .collect(Collectors.toList());
         System.out.println("Urgency high : " + deadlines);
+
+
+
+        List<String> docsIds2 = documents.stream()
+                .filter(document -> document.getStatus() == IN_PROGRESS)
+                .map(document -> document.getDocumentId())
+                .collect(Collectors.toList());
+        List<Long> until = jobs.stream()
+                .filter(job -> docsIds2.contains(job.getDocumentId()))
+                .map(job -> LocalDateTime.now().until(job.getDeadLine(), ChronoUnit.DAYS))
+                .collect(Collectors.toList());
+        System.out.println(until);
     }
 }
